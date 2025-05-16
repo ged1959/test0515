2cd8b6bc6880173cf1d61293b3d47d9a9e9774de
yik <y.ikawa@yoika.net>
2025-05-16 09:34:18 +0900

    first commit

---
+++ test/src/components/Test.vue
@@ -0,0 +1,3 @@
+<template>
+    <h2>変更の確認てすと</h2>
+</template>
--- test/src/components/TheWelcome.vue
+++ test/src/components/TheWelcome.vue
@@ -5,6 +5,7 @@ import ToolingIcon from './icons/IconTooling.vue'
 import EcosystemIcon from './icons/IconEcosystem.vue'
 import CommunityIcon from './icons/IconCommunity.vue'
 import SupportIcon from './icons/IconSupport.vue'
+import Test from './Test.vue'
 
 const openReadmeInEditor = () => fetch('/__open-in-editor?file=README.md')
 </script>
@@ -20,6 +21,8 @@ const openReadmeInEditor = () => fetch('/__open-in-editor?file=README.md')
     <a href="https://vuejs.org/" target="_blank" rel="noopener">official documentation</a>
     provides you with all information you need to get started.
   </WelcomeItem>
+
+  <Test />
 
   <WelcomeItem>
     <template #icon>
